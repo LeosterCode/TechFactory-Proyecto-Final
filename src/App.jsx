0d3a0c1086352971c87bbd703e52110e2d8e952e
LeosterCode <164199518+LeosterCode@users.noreply.github.com>
2025-03-04 23:02:16 -0600

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './App.css'
 import Navbar from './components/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 export default function Navbar() {
-  return (
-    <div>navbar</div>
-  )
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() =>{
+        const handlescroll = ()=>{
+            setScrolled(window.scrollY >100);
+        };
+        window.addEventListener('scroll', handlescroll);
+        return () => window.removeEventListener('scroll', handlescroll);
+    },[]);
+    return (
+           <nav className={`navbar navbar-expand-sm navbar-light bg-transparent ${scrolled ? 'bg-dark' : ''}`}>
+               <div className="container-fluid">
+                   <a className="navbar-brand" href="#">primary</a>
+                   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                       aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                       <span className="navbar-toggler-icon"></span>
+                   </button>
+                   <div className="collapse navbar-collapse" id="navbarID">
+                       <div className="navbar-nav">
+                           <a className="nav-link active" aria-current="page" href="#">Sobre Nosotros</a>
+                           <a className="nav-link active" aria-current="page" href="#">Que es TechFactory</a>
+                           <a className="nav-link active" aria-current="page" href="#">Descargar</a>
+                           <a className="nav-link active" aria-current="page" href="#">Instruccionnes de uso</a>
+                           <a className="nav-link active" aria-current="page" href="#">Dedicatorias</a>
+                       </div>
+                   </div>
+               </div>
+           </nav>
+
+           
+    )
 }
