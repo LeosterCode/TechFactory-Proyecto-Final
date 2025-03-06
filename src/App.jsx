ddb221ce0836be5f7f18f73ec80b83adae827c08
LeosterCode <164199518+LeosterCode@users.noreply.github.com>
2025-03-06 11:54:26 -0600

    Primeros pasos footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css'
 import Navbar from './components/navbar'
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
     <Navbar/>
     <Header/>
     <section className='vh-100'></section>
+    <Footer/>
     </>
   )
 }
