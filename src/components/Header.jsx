5df825c2a78e653b1b4e5fd8660cbb2a150a850e
LeosterCode <164199518+LeosterCode@users.noreply.github.com>
2025-03-05 23:00:46 -0600

    animacion header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React,{useEffect, useRef} from 'react'
 import icono from "./../assets/iconoTransparentTF.svg"
 
+
 export default function Header() {
+    const headerRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+          ([entry]) => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add("visible");
+            }
+          },
+          { threshold: 0.5 }
+        );
+    
+        if (headerRef.current) {
+          observer.observe(headerRef.current);
+        }
+    
+        return () => {
+          if (headerRef.current) {
+            observer.unobserve(headerRef.current);
+          }
+        };
+      }, []);
     return (
         <header className='header vh-100 d-flex flex-column justify-content-center align-items-center'>
-            <div className='text-center'>
-                <img src={icono} alt="Icono de TechFatory" />
+            <div ref={headerRef} className='text-center'>
+                <img src={icono} alt="Icono de TechFatory" className='logo' />
                 <h1 className='text-gradiant'>TechFactory</h1>
             </div>
         </header>
