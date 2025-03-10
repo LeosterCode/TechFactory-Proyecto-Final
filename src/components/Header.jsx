7f54f67d84d6da13f7828f1d110657821f52b9f8
LeosterCode <164199518+LeosterCode@users.noreply.github.com>
2025-03-10 08:51:01 -0600

    Arreglando erroes del deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect, useRef} from 'react'
-import icono from "../assets/iconoTransparentTF.svg"
+import icono from "../assets/IconoTransparentTF.svg"
 
 
 export default function Header() {
