63ab6ec1121ec506e77ae304b397f310fdbb3d76
AdanCodi <promikculi@gmail.com>
2025-03-11 10:35:48 -0600

    nuevo diseño seccion_us

--- src/components/SobreUS.jsx
+++ src/components/SobreUS.jsx
@@ -2,41 +2,48 @@ import React from "react";
 
 const SobreUs = () => {
   return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <h1>Sobre Nosotros</h1>
-      <p>
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-        pruebas pruebas pruebas pruebas pruebas pruebas pruebas
-      </p>
+    <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src={icono} alt="Icono de TechFatory" className='logo' />
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Encargad@ de la Barra de navegacion</h5>
+        <p>Estudiante de Desarrolo de Software Multiplataforma</p>
+        <p>Contactos:</p>
+        <ul>
+          <li>Contactos:</li>
+        </ul>
+      </div>
     </div>
+    <div class="carousel-item">
+      <img src={icono} alt="Icono de TechFatory" className='logo' />
+      <div class="carousel-caption d-none d-md-block">
+      <h5>Encargad@ de la Barra de navegacion</h5>
+      <p>Estudiante de Desarrolo de Software Multiplataforma</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src={icono} alt="Icono de TechFatory" className='logo' />
+      <div class="carousel-caption d-none d-md-block">
+      <h5>Encargad@ de la Barra de navegacion</h5>
+      <p>Estudiante de Desarrolo de Software Multiplataforma</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
   );
 };
 
